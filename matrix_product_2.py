81f1f357cfb2c835ba4a330b330eafa25a01220d
Fabian Roh <fabiankmroh@gmail.com>
2018-01-28 12:28:34 +0900

    Code refined and comment added.

--- matrix_product_2.py
+++ matrix_product_2.py
@@ -9,24 +9,23 @@ b_row = int(input("B Column:: "))
 if(a_row != b_col):
     print("ERROR")
 else:
+    #creating the size of the result according to the columns and rows of a and b
     result = [[0 for x in range(b_row)] for y in range(a_col)]
-    print(result)
     np.random.seed(1)
 
+    #creating random arrays according to the columns and rows of a and b
     a = np.random.randn(a_col*a_row).reshape(a_col,a_row)
-    print(a)
-    print("\n")
     b = np.random.randn(b_col*b_row).reshape(b_col,b_row)
-    print(b)
-    print("\n")
 
-
-    for i in range(a_col):
-        for j in range(b_row):
+    for i in range(len(a)): #going through the # of rows
+        for j in range(len(b[0])): #going through the # of columns
             temp_add = 0
-            for k in range(a_row):
+            for k in range(len(b)):
                 temp_add += a[i][k] * b[k][j]
+            #adding the multiplied data to the final array
             result[i][j] = temp_add
+
+    #printing calculated result and original correct answers to compare
     print(result)
     print("\n")
     correct_answer = a.dot(b)
