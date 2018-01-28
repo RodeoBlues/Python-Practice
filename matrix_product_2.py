07773ffddabc84a2cef8630b44267bf0b270cb25
Fabian Roh <fabiankmroh@gmail.com>
2018-01-28 12:06:34 +0900

    Error fixed.

--- matrix_product_2.py
+++ matrix_product_2.py
@@ -1,14 +1,16 @@
 import numpy as np
 
-a_col = int(input("A Column:: "))
-a_row = int(input("A Row:: "))
-b_col = int(input("B Column:: "))
-b_row = int(input("B Row:: "))
+a_col = int(input("A Row:: "))
+a_row = int(input("A Column:: "))
+
+b_col = int(input("B Row:: "))
+b_row = int(input("B Column:: "))
 
 if(a_row != b_col):
     print("ERROR")
 else:
-    result = [[0 for x in range(a_col)] for y in range(b_row)]
+    result = [[0 for x in range(b_row)] for y in range(a_col)]
+    print(result)
     np.random.seed(1)
 
     a = np.random.randn(a_col*a_row).reshape(a_col,a_row)
@@ -18,14 +20,14 @@ else:
     print(b)
     print("\n")
 
-    haedap = a.dot(b)
 
     for i in range(a_col):
         for j in range(b_row):
             temp_add = 0
             for k in range(a_row):
                 temp_add += a[i][k] * b[k][j]
-            result[i][j] += temp_add
+            result[i][j] = temp_add
     print(result)
     print("\n")
-    print(haedap)
+    correct_answer = a.dot(b)
+    print(correct_answer)
